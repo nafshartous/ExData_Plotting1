823ec6baaf0045c86b3ecc1acf1fb5467477cdbd
nafshartous <nick@afshartous.com>
2017-02-18 10:17:53 -0500

    Use na.strings

--- plot1.R
+++ plot1.R
@@ -1,11 +1,11 @@
-df = read.table("household_power_consumption.txt", header=TRUE, sep=";", dec = ".")
+df = read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
 
 df2 = df[ which(df$Date=='1/2/2007' | df$Date=='2/2/2007'), ]
 
-z = transform(df2, Global_active_power = as.numeric(Global_active_power))
+df3 = na.omit(df2)
 
-hist(z$Global_active_power, xlab="Global Active Power (kilowatts)", 
-     ylab="Frequency", col="Red", main="Global Active Power") 
+hist(df3$Global_active_power, xlab="Global Active Power (kilowatts)", 
+      ylab="Frequency", col="Red", main="Global Active Power") 
 
 dev.copy(png,'plot1.png')
 dev.off()
